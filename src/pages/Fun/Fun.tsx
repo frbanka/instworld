9a1867b800766dd2747e22870c1a36002eb5b27a
Jacek Wójciak <fr.banka@gmail.com>
2024-01-04 21:54:53 +0100

    donate

--- src/pages/Fun/Fun.tsx
+++ src/pages/Fun/Fun.tsx
@@ -1,7 +1,86 @@
+import { NavLink } from "react-router-dom";
+import "./Fun.css";
 export const Fun = () => {
   return (
-    <div className="container">
-      <h1>access denied - members only</h1>
+    <div className="funcontainer">
+      <div className="funtext">
+        <div className="funsection1">
+          <div className="h12 p12">The Money Counter:</div>
+          <p className="h4 p3">
+            you help me get <a className="h3">0 $</a>, updated day 5/01/2024 on
+            20:00
+          </p>
+        </div>
+        <div className="funsection4">
+          <p className="h1 p4">Important!</p>
+          <p className="h4 p3">
+            Every single donate will be listed on this website (on Hall of Fame
+            page) - with a donors name, nick, instagram nickname, link to your
+            youtube channel, link to any of your social profile (even your OF
+            page) - whatever you leave me in the donate feedback. <br></br>
+            You can send me your photo too.
+          </p>
+        </div>
+        <div className="funsection2">
+          <p className="h1 p4">Second thought</p>
+          <p className="h3 p5">
+            My wife told me, that this experiment cannot have the character of
+            crowdfunding or alms
+          </p>
+          <p className="h4 p5">
+            In other words, I need to tell you this: I'm not poor, I'm not
+            begging for money, I'm not gonna create or sell anything here and
+            I'm not trying to trick you, extort money from you. I do not
+            organize any services here, this is not a financial pyramid or
+            business proposal. This is not charity collection, I don't work for
+            any commercial company, any advertising or model agency. All I mean
+            is that you can do whatever you want - you can participate to this
+            experiment by donating or don't.
+          </p>
+          <p className="h3 p5">
+            If you are still interesed in donating, keep scrolling
+          </p>
+        </div>
+        <div className="funsection3">
+          <p className="h2 p3">
+            But, your donation cannot be completly for nothing, so I came up
+            with this:
+          </p>
+          <p className="p4 h2">I've created several pieces of music</p>
+          <p className="p4 h3">And you can just support me and my work</p>
+          <p className="p4 h3">You can listen to it here:</p>
+        </div>
+        <div className="funsection4">
+          <p className="h12 p2">And here is how you can participate:</p>
+        </div>
+        <div className="funsection5">
+          <p className="p5 h3">Free:</p>
+          <p className="p5 h4">
+            Just share this website or some of my posts on Instagram or X: posts
+          </p>
+          <p className="p5 h4">Like, comment and follow me on Instagram or X</p>
+          <p className="p5 h3">Donate:</p>
+          <p className="p5 h4">PayPal</p>
+          <p className="p5 h4">Buymeacoffee</p>
+          <p className="p5 h4">Donorbox</p>
+          <p className="p5 h4">
+            Crypto<br></br>
+            <a className="h5">
+              (since the value of crypto changes very fast - this is the least
+              preffered option)
+            </a>
+          </p>
+        </div>
+        <div className="funsection3">
+          <p className="h2 p3">Contact with me</p>
+          <p className="p4 h3">e-mail: themilliondollarsexperiment@gmail.com</p>
+          <p className="p4 h3">instagram: youmademerich</p>
+          <p className="p4 h3">twitter/X: @bankaplay</p>
+        </div>
+        <div className="funsection10">
+          <p className="p2 h5">#onemilliondollarman</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/MyWork/MyWork.tsx
+++ src/pages/MyWork/MyWork.tsx
@@ -5,11 +5,12 @@ export const MyWork = () => {
     <div className="workcontainer">
       <div className="worktext">
         <div className="worksection1">
-          <div className="h12 p12">The main goal is 1 million $</div>
+          <div className="h12 p12">Target is 1 million $</div>
           <p className="h3 p3">
-            I am not sure if we will reach a million here or just few bucks..
-            but maybe we will reach even more, like few millions.. Thats why I
-            came up with some different stages, thresholds - Goals
+            I am not sure if we will reach a million or just few bucks.. but
+            maybe we will reach even more, like few millions.. Thats why I came
+            up with some different stages, thresholds - things I will do after I
+            get specific amount - The Goals
           </p>
         </div>
         <div className="worksection4">
@@ -25,24 +26,18 @@ export const MyWork = () => {
         <div className="worksection2">
           <p className="h1 p4">The Goals</p>
           <p className="h3 p5">
-            They're thing I will do after getting a specific amount of money
+            There are things I will do after getting a specific amount of money
           </p>
-          <p className="h2 p5">Why this is not a crowdfunding page? See FAQ</p>
+          <p className="h2 p5">Things I will buy using donated money</p>
         </div>
         <div className="worksection3">
           <p className="h2 p3">
-            Coming up with the Goals I wanted it to be something that gives you
-            a REAL feeling of being a part of this experiment
+            Coming up with the Goals I wanted it to be something that makes this
+            experiment fun and worth following later
           </p>
           <p className="p4 h3">
-            Something that you can use, really like or maybe interact with.
-          </p>
-          <p className="p4 h4">
-            It has to be the something very fun and entertaining for You
-          </p>
-          <p className="p4 h3">
-            Similar to those things You can get from other Creators, but a
-            little bit more.. interactive
+            Things that will change my life. From regular person to someone who
+            can effort anything
           </p>
         </div>
         <div className="worksection4">
@@ -54,34 +49,105 @@ export const MyWork = () => {
           <p>
             <ul className="h5">
               <li>
-                What can I do for 10$? Not much, you say..?<br></br>FALSE!
-                <br></br>The first donations will be the most important
-                donations of all. After the experiment end, the first donators
-                will receive a personal gift from me, including a postcard sent
-                from the first vacation I will go to, and, what's more
-                important, I will meet with them in person (we'll work out the
-                details later).<br></br>Let's say - the first 5 people who make
-                a donate at least 2$ value<br></br>(2$ multiply by 5 is 10$)
+                What I will do with 10$? Not much, you say..?<br></br>FALSE!
+                <br></br>I will use it to buy postards. Yes, postcards. After
+                the experiment end, the first donators will receive a personal
+                gift from me, including a postcard sent from the first vacation
+                I will go to, and, what is more important, I will meet with them
+                in person (we'll work out the details later).<br></br>Let's say
+                - the first donations that reaches 10$ (if the very first
+                donation will be 10$ - then this one person wins, if there will
+                be 5 donations of 2$ - five persons wins).<br></br> But here's
+                the catch.<br></br>I will update The Money Counter after it
+                reaches 500$ - so till then everybody can be that first donation
+                - either you are the first or there was many others before you
               </li>
             </ul>
           </p>
-          <p className="p5 h3">Then - 100$</p>
-          <p>
-            <ul className="h5">
-              <li>
-                To make this more fair - every donate made of value 10$ or more
-                will get a postcard from me
-              </li>
-              <li>After I get 100$ from all donations</li>
-            </ul>
-          </p>
+          <p className="p5 h3">Then - 500$</p>
+          <ul className="h5">
+            <li>I will announce the first donators (10$ goal)</li>
+            <li>I will buy a new toy for my dog and cat</li>
+          </ul>
           <p className="p5 h3">Maybe - 1.000$ ?</p>
+          <ul className="h5">
+            <li>I will take my wife to expensive restaurant for dinner</li>
+            <li>I will buy myself a new smartphone. Mine's old</li>
+            <li>
+              I will take my wife on a roadtrip and spend few nights in
+              beautiful place
+            </li>
+          </ul>
           <p className="p5 h3">Something bigger - 10.000$</p>
+          <ul className="h5">
+            <li>
+              Get myself a new TV or a computer. Or fridge. Or new dinning
+              table. Or all of it? You'll help me pick
+            </li>
+            <li>
+              Fix my car - some paintjobs, parts replacing, deep cleaning..{" "}
+            </li>
+            <li>
+              Buy some new, more expensive clothes from shops I do not normally
+              go
+            </li>
+          </ul>
           <p className="p5 h3">Is this real? - 100.000$</p>
+          <ul className="h5">
+            <li>I will buy a suit. From well known brand - you'll pick</li>
+            <li>
+              I will take my wife on few exotic vacation (one in Croatia for
+              sure) and buy whatever we like there
+            </li>
+            <li>
+              Get me a motorbike (I always wanted to have one). And drive it
+              with my girrrlll
+            </li>
+          </ul>
           <p className="p5 h3">Nope. We never get here - 1.000.000$</p>
+          <ul className="h5">
+            <li>
+              Since it is almost impossible.. and it is the end of The
+              Experiment.. I will show my real face on Instagram
+            </li>
+            <li>
+              From now on - I am a famous, well known person - I will start
+              acting like one. I will try at least
+            </li>
+            <li>I will post more of my private life on Instagram</li>
+            <li>
+              I will start sending postcards to every donators (every who
+              donated more then 5$)
+            </li>
+            <li>I will meet with chosen donators</li>
+            <li>I will buy a new house</li>
+            <li>Buy a new car. One for me and one for somebody of my family</li>
+            <li>I will pay all debts of my family and close friends</li>
+            <li>
+              I will travel the world with my wife. First, we'll visit Scotland
+              (we always wanted). <br></br>Then maybe Spain, Italy, Greece,
+              Netherlands and Norway (random order)
+            </li>
+          </ul>
           <p className="p5 h3">Nope x2 - 2.000.000$</p>
+          <ul className="h5">
+            <li>
+              I will quit my regular job. And do it in a way You will pick
+            </li>
+            <li>I will quit my job. And do it in a way You will pick</li>
+            <li>I will quit my job. And do it in a way You will pick</li>
+          </ul>
           <p className="p5 h3">In the middle of insanity - 5.000.000$</p>
           <p className="p5 h3">Just for fun - 10.000.000$</p>
+          <ul className="h5">
+            <li>
+              I will buy a land and house in Croatia and start new life in there
+            </li>
+            <li>I will buy more cars - maybe someone will get one..?</li>
+            <li>
+              Travel the world with my wife pt.2 - spend a month in America
+            </li>
+          </ul>
           <p className="p5 h3">More? Just stop..</p>
         </div>
         <div className="worksection10">
