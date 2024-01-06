5502f0809eee7744bd9a47179944f6ce4b20b509
Jacek Wójciak <fr.banka@gmail.com>
2024-01-06 19:38:03 +0100

    uppp

--- src/pages/Fun/Fun.tsx
+++ src/pages/Fun/Fun.tsx
@@ -7,47 +7,55 @@ export const Fun = () => {
         <div className="funsection1">
           <div className="h12 p12">The Money Counter:</div>
           <p className="h4 p3">
-            you help me get <a className="h3">0 $</a>, updated day 5/01/2024 on
-            20:00
+            you helped me get <a className="h3">0 $</a>, updated day 7/01/2024
+            on 20:00
           </p>
         </div>
         <div className="funsection4">
-          <p className="h1 p4">Important!</p>
-          <p className="h4 p3">
-            Every single donate will be listed on this website (on Hall of Fame
-            page) - with a donors name, nick, instagram nickname, link to your
-            youtube channel, link to any of your social profile (even your OF
-            page) - whatever you leave me in the donate feedback. <br></br>
-            You can send me your photo too.
+          <p className="h3 p4">If you want to donate - click here</p>
+          <p className="h4 p5">
+            If you want to read more about the project - The Project
           </p>
+          <p className="h4 p5">If you need some answers - FAQ</p>
+          <p className="h4 p5">If you still need some answers - Contact</p>
         </div>
         <div className="funsection2">
-          <p className="h1 p4">Second thought</p>
-          <p className="h3 p5">
-            My wife told me, that this experiment cannot have the character of
-            crowdfunding or alms
-          </p>
+          <p className="h1 p4">Last thought</p>
           <p className="h4 p5">
+            My wife noticed, that this project cannot have the character of
+            crowdfunding or worst - the alms
+          </p>
+          <p className="h5 p5">
             In other words, I need to tell you this: I'm not poor, I'm not
-            begging for money, I'm not gonna create or sell anything here and
-            I'm not trying to trick you, extort money from you. I do not
-            organize any services here, this is not a financial pyramid or
-            business proposal. This is not charity collection, I don't work for
-            any commercial company, any advertising or model agency. All I mean
-            is that you can do whatever you want - you can participate to this
-            experiment by donating or don't.
+            begging for money, I'm not trying to trick you, extort money from
+            you, you won't get any goods from me (except some gifts described in
+            the goals). I do not offer any services here, this is not a
+            financial pyramid or business proposal. I am not organising any
+            lottery, now or later. This is not charity collection, I don't work
+            for any commercial company, any advertising or model agency or
+            television. I do not cooperate with any of well-known media person.
+            This project was only my idea. All this means, that you can (by your
+            choice) donate some money on this website, but don't expect nothing
+            in return. Some things I described and promised to do in The Goals
+            page may not be done due to the unforeseen events. But I can promise
+            to do my best to keep my word on that. You can participate to this
+            project not only by donating, but by sharing this page, sharing my
+            posts or following my Instagram account
           </p>
           <p className="h3 p5">
-            If you are still interesed in donating, keep scrolling
+            If you are still willing to donate, keep scrolling
           </p>
         </div>
         <div className="funsection3">
           <p className="h2 p3">
-            But, your donation cannot be completly for nothing, so I came up
-            with this:
+            To make donations more sense, I came up with this:
           </p>
-          <p className="p4 h2">I've created several pieces of music</p>
-          <p className="p4 h3">And you can just support me and my work</p>
+          <p className="p4 h3">
+            Some time ago I have created several pieces of music (I used them in
+            my small game-project) and I am giving them for free on this
+            website. So you can now support me for my work! Feel free to use it.
+          </p>
+          <p className="p4 h3"></p>
           <p className="p4 h3">You can listen to it here:</p>
         </div>
         <div className="funsection4">
@@ -64,10 +72,10 @@ export const Fun = () => {
           <p className="p5 h4">Buymeacoffee</p>
           <p className="p5 h4">Donorbox</p>
           <p className="p5 h4">
-            Crypto<br></br>
+            Crypto - Bitcoin<br></br>
             <a className="h5">
               (since the value of crypto changes very fast - this is the least
-              preffered option)
+              preffered option and will be offline for now)
             </a>
           </p>
         </div>
--- src/pages/MyWork/MyWork.tsx
+++ src/pages/MyWork/MyWork.tsx
@@ -23,10 +23,10 @@ export const MyWork = () => {
             Even your photo.
           </p>
           <p className="h4 p3">
-            And remember from the beginning - you don't have to pay me, you can
-            just follow me on Instagram or X. Share this website. Write to me.
-            Or none of this, do nothing. Whatever you choose - I support your
-            choice.
+            And remember - you don't have to pay me, you can just follow me on
+            Instagram or X. Share this website. Write to me. Or none of this, do
+            nothing. Whatever you choose - I support your choice and thank you
+            for visiting the page.
           </p>
         </div>
         <div className="worksection2">
@@ -36,8 +36,9 @@ export const MyWork = () => {
             money. Things I always wanted to do, but I couldn't affort it.
           </p>
           <p className="h3 p5">
-            Especially, things I will buy using donated money. Things that will
-            change me and my life and my friends life.
+            Especially, things I want to buy using donated money. Things that
+            will change my life, my family and my friends life. And you will see
+            how this changes are done
           </p>
         </div>
         <div className="worksection3">
@@ -46,121 +47,250 @@ export const MyWork = () => {
             experiment fun and worth following later.
           </p>
           <p className="p4 h3">
-            Something that will make your participation and generosity, lets
-            say, worth the effort.
+            Something that will make your participation and generosity worth it.
           </p>
         </div>
         <div className="worksection4">
           <p className="h12 p2">So here are the Goals</p>
           <p className="p5 h4">
-            You can follow their realization on my social media profiles.
+            You will be able to follow their realization on my social media
+            profiles.<br></br>It will be in a form of photo or video.
+          </p>
+          <p className="p5 h4">
+            I am open to ideas.<br></br>Write to me what I can do or buy - the
+            most repeatable ideas will be added
           </p>
         </div>
         <div className="worksection5">
-          <p className="p5 h3">Starting with 10$</p>
+          <p className="p4 h3">Starting with 10$</p>
           <p>
             <ul className="h5">
               <li>
-                What I will do with 10$? Not much, you say..?<br></br>FALSE!
-                <br></br>I will use it to buy postards. Yes, postcards. After
-                the experiment end, the first donators will receive a personal
-                gift from me, including a postcard sent from the first vacation
-                I will go to, and, what is more important, I will meet with them
-                in person (we'll work out the details later).<br></br>Let's say
-                - the first donations that reaches 10$ (if the very first
-                donation will be 10$ - then this one person wins, if there will
-                be 5 donations of 2$ - five persons wins).<br></br> But here's
-                the catch.<br></br>I will update The Money Counter after it
-                reaches 500$ - so till then everybody can be that first donation
-                - either you are the first or there was many others before you
+                What I will do with 10$? Not much, you say?<br></br>FALSE! It is
+                very important goal!
+                <br></br>I will use this 10$ some day to buy postards. Yes,
+                postcards.<br></br>After the 10.000$ goal will be reached, the
+                first donators will receive a personal gift from me, including a
+                postcard sent from the first vacation I will go to. And then,
+                what is more important, I will meet with them in person - this
+                last one will be done after experiment end (1 million $
+                reached).<br></br>The first donations that sums up to 10$ will
+                get the reward (if the very first donation will be 10$ - then
+                this one person wins, if there will be 5 donations of 2$ - five
+                persons wins etc.).
+                <br></br> But here's the catch.
+                <br></br>The first update of The Money Counter will be done
+                after it reaches 500$ - so till then nobody will know how many
+                donations were made and anybody still can be the first one and
+                win.
               </li>
             </ul>
           </p>
-          <p className="p5 h3">Then - 500$</p>
+          <p className="p5 h3">500$</p>
+          <p>
+            <ul className="h5">
+              <li>I will announce the first donators (10$ goal)</li>
+              <li>I will put the first donators on the Hall of Fame page</li>
+              <li>
+                (From now on, every donator will be added to Hall of Fame page
+                with everyday update)
+              </li>
+              <li>
+                I will share more information about myself (like my first name,
+                age, my interests etc.)
+              </li>
+              <li>I will order few pizzas and eat it with friends</li>
+              <li>I will buy my wife something nice - earings, shoes?</li>
+              <li>I will buy myself new shoes</li>
+              <li>
+                I will buy new toys for my dog and cat and post a photo of them
+                (my pets)
+              </li>
+              <li>
+                I will buy few local lottery scratch cards and add all the
+                winnings to The Money Counter
+              </li>
+            </ul>
+          </p>
+          <p className="p5 h3">1.000$</p>
           <ul className="h5">
-            <li>I will announce the first donators (10$ goal)</li>
-            <li>I will buy a new toy for my dog and cat</li>
+            <li>
+              I will take the photo of my... hands. And post it on Instagram
+            </li>
+            <li>I will answer few non-private questions (make a Q&A)</li>
+            <li>
+              I will go shopping with my wife and buy some unnecessary things
+            </li>
+            <li>I will buy my wife some beautiful jewelry</li>
+            <li>I will buy myself a new smartphone</li>
+            <li>
+              I will take my wife on a trip with a stay on a weekend in a some
+              hotel
+            </li>
+            <li>I will take my wife to expensive dinner</li>
           </ul>
-          <p className="p5 h3">Maybe - 1.000$ ?</p>
+          <p className="p5 h3">10.000$</p>
           <ul className="h5">
-            <li>I will take my wife to expensive restaurant for dinner</li>
-            <li>I will buy myself a new smartphone. Mine's old</li>
-            <li>
-              I will take my wife on a roadtrip and spend few nights in
-              beautiful place
+            <li>I will post more information about me</li>
+            <li>I will post a photo of my.. car.</li>
+            <li>I will post a photo of.. (you'll choose)</li>
+            <li>Buy a new TV and computer.</li>
+            <li>Make a small home renovation</li>
+            <li className="h6">
+              (This is getting so much harder - the more money I'll get the less
+              I know what to do with it..<br></br>so send me some ideas please!)
             </li>
+
+            <li>Buy more jewelry for my wife.</li>
+            <li>
+              Take my whole family for shopping and let them buy whatever they
+              want
+            </li>
+            <li>
+              Buy myself some really expensive clothes (from shops I do not
+              normally go to)
+            </li>
+            <li>
+              I will buy an expensive whisky, maybe{" "}
+              <a href="https://www.wine-searcher.com/find/the+macallan+eighteen+old+sherry+oak+single+malt+scotch+whisky+speyside+highlands+scotland">
+                this one
+              </a>{" "}
+              (but you'll help me choose) and make a review of it
+            </li>
+            <li>
+              I will renovate my old car - do some paintjob, replace parts, buy
+              new wheels etc.
+            </li>
+            <li>
+              I will put a sticker on my car, saying 'sponsored by xxx' (xxx -
+              the name/nick of randomly picked donator).
+            </li>
+            <li>Go on vacation! You will help me choose where exactly</li>
+            <li>Send personal gifts and postcards to first donators</li>
+            <li>Make a photo report from vacation on Instagram</li>
           </ul>
-          <p className="p5 h3">Something bigger - 10.000$</p>
+          <p className="p5 h3">100.000$</p>
           <ul className="h5">
             <li>
-              Get myself a new TV or a computer. Or fridge. Or new dinning
-              table. Or all of it? You'll help me pick
+              More about me - I will tell you where I live, where I am from
             </li>
             <li>
-              Fix my car - some paintjobs, parts replacing, deep cleaning..{" "}
+              Make another Q&A, but with a private questions, some of them I
+              will asnwer
             </li>
+            <li>I will post a photo of whole me (but yet no face)</li>
+            <li>I will have a phone call with one randomly pick donator</li>
             <li>
-              Buy some new, more expensive clothes from shops I do not normally
-              go
+              I will start making some live streams on Twitch or Instagram (with
+              no specific content)
             </li>
+            <li>I will buy a new, expensive suit</li>
+            <li>I will share a photo of me in that suit</li>
+            <li>
+              Go with my wife on few exotic vacations (you'll help me pick) and
+              we buy some stuff in there (with your help ofc)
+            </li>
+            <li>I will buy a motorbike. And drive on it with my girrrlll</li>
+            <li>I will buy new smartphone for everyone in my family</li>
+            <li>I will take my family and friends for BIG SHOPPING</li>
+            <li>I will spend 5.000$ in one night on drinks in my city</li>
+            <li>
+              I will take my family and lot of friends to very good expensive
+              restaurant nerby
+            </li>
+            <li>I will make a big party with lot of alcohol</li>
+            <li>
+              I will buy 1000 lottery scratch cards and 1000 lottery tickets and
+              add all the winnings to The Money Counter
+            </li>
+            <li>Finally.. I will buy a new car</li>
           </ul>
-          <p className="p5 h3">Is this real? - 100.000$</p>
-          <ul className="h5">
-            <li>I will buy a suit. From well known brand - you'll pick</li>
-            <li>
-              I will take my wife on few exotic vacation (one in Croatia for
-              sure) and buy whatever we like there
-            </li>
-            <li>
-              Get me a motorbike (I always wanted to have one). And drive it
-              with my girrrlll
-            </li>
-          </ul>
-          <p className="p5 h3">Nope. We never get here - 1.000.000$</p>
+          <p className="p5 h3">1.000.000$</p>
           <ul className="h5">
             <li>
-              Since it is almost impossible.. and it is the end of The
-              Experiment.. I will show my real face on Instagram
+              Since it is a lot of money and it is the official end of the
+              experiment..
+            </li>
+            <li>I will show myself on Instagram</li>
+            <li>
+              From now on I am a famous, well known person (I guess) and I will
+              start acting like one.
             </li>
             <li>
-              From now on - I am a famous, well known person - I will start
-              acting like one. I will try at least
+              I'll start posting more photos and stories of my rich life on
+              Instagram
             </li>
-            <li>I will post more of my private life on Instagram</li>
             <li>
-              I will start sending postcards to every donators (every who
-              donated more then 5$)
+              I will start an everyday series on Instagram or Facebook called
+              'things that rich people buy everyday'
+            </li>
+            <li>I will record a song</li>
+            <li>I will make a money related tattoo</li>
+            <li>
+              I will open my own merch fan shop (with clothes for example)
+            </li>
+            <li>
+              I will be sending postcards to every donators who donated more
+              then 5$
             </li>
             <li>I will meet with chosen donators</li>
-            <li>I will buy a new house</li>
-            <li>Buy a new car. One for me and one for somebody of my family</li>
-            <li>I will pay all debts of my family and close friends</li>
+            <li>I will buy a new house or apartment</li>
+            <li>Buy some new cars. For a family members or a friends</li>
+            <li>I will give away my old car (the one with the sticker)</li>
             <li>
-              I will travel the world with my wife. First, we'll visit Scotland
-              (we always wanted). <br></br>Then maybe Spain, Italy, Greece,
-              Netherlands and Norway (random order)
+              I will start travelling the world with my wife. First, we'll go to
+              Scotland (we always wanted).
+            </li>
+            <li>
+              Then maybe Spain, Italy, Greece, Netherlands, Norway, USA (random
+              order and you will pick)
             </li>
           </ul>
-          <p className="p5 h3">Nope x2 - 2.000.000$</p>
+          <p className="p5 h3">2.000.000$</p>
           <ul className="h5">
             <li>
-              I will quit my regular job. And do it in a way You will pick
+              If donations will keep going.. I think I will quit my job. And
+              start to live only from profits of my popularity
             </li>
-            <li>I will quit my job. And do it in a way You will pick</li>
-            <li>I will quit my job. And do it in a way You will pick</li>
           </ul>
-          <p className="p5 h3">In the middle of insanity - 5.000.000$</p>
-          <p className="p5 h3">Just for fun - 10.000.000$</p>
+          <p className="p5 h3">5.000.000$</p>
           <ul className="h5">
             <li>
-              I will buy a land and house in Croatia and start new life in there
+              With this amount of money I can realize every dream I ever had..
             </li>
-            <li>I will buy more cars - maybe someone will get one..?</li>
+            <li>Buy a land and house abroad and start new life with family</li>
+            <li>Buy retro-classic-vintage car I always wanted</li>
             <li>
-              Travel the world with my wife pt.2 - spend a month in America
+              Buy always-wanted-to-have smaller things (like telescope or pool
+              table in a house)
+            </li>
+            <li>Travel the world pt.2 - spend few weeks in America</li>
+            <li>
+              Visit some exotic islands (Maldives, Tahiti, Galapagos, Bora Bora,
+              Caymans, Canary)
+            </li>
+            <li>
+              I will invite 10 random donators with one companion to my new
+              house (I'll pay all the travel costs)
             </li>
           </ul>
-          <p className="p5 h3">More? Just stop..</p>
+          <p className="p5 h3">10.000.000$</p>
+          <ul className="h5">
+            <li>No idea what to do. Anybody..? Maybe buy tickets to space?</li>
+            <li>
+              I could buy more expensive whisky and review it,{" "}
+              <a href="https://www.wine-searcher.com/find/the+macallan+reach+81+old+single+malt+scotch+whisky+highlands+speyside+scotland">
+                like this one
+              </a>{" "}
+              , or..
+            </li>
+            <li>
+              Make/host a big party with internet celebrities - something like
+              comic con - to gather every possible youtuber,<br></br>streamer or
+              model to meet with their fans
+            </li>
+          </ul>
+          <p className="p5 h3">What else I can do or buy? Write to me!</p>
         </div>
         <div className="worksection10">
           <p className="p2 h5">#onemilliondollarman</p>
